198d79835b6a2ca1859c88b6472a6b4a236629ae
David Dunn <davdunn2@uk.ibm.com>
2018-04-17 12:43:35 +0100

    More updates

--- Sources/Kitura/RouterResponse.swift
+++ Sources/Kitura/RouterResponse.swift
@@ -361,6 +361,16 @@ public class RouterResponse {
         return send(renderedResource)
     }
     
+    public func render<T: Codable>(_ resource: String, using value: T,
+                                   options: RenderingOptions = NullRenderingOptions()) throws -> RouterResponse {
+        guard let router = getRouterThatCanRender(resource: resource) else {
+            throw TemplatingError.noTemplateEngineForExtension(extension: "")
+        }
+        
+        let renderedResponse = try router.render(template: resource, context: value)
+        return send(renderedResponse)
+    }
+    
     public func render<T: Codable>(_ resource: String, context: T, forKey key: String,
                                    options: RenderingOptions = NullRenderingOptions()) throws -> RouterResponse {
         guard let router = getRouterThatCanRender(resource: resource) else {
@@ -380,6 +390,17 @@ public class RouterResponse {
         let renderedResponse = try router.render(template: resource, context: context, forKey: key)
         return send(renderedResponse)
     }
+    
+    public func render<T: Codable>(_ resource: String, context: [String: [T]],
+                                   options: RenderingOptions = NullRenderingOptions()) throws -> RouterResponse {
+        guard let router = getRouterThatCanRender(resource: resource) else {
+            throw TemplatingError.noTemplateEngineForExtension(extension: "")
+        }
+        
+        let renderedResponse = try router.render(template: resource, context: context)
+        return send(renderedResponse)
+    }
+
     
     /// Render a resource using Router's template engine.
     ///
